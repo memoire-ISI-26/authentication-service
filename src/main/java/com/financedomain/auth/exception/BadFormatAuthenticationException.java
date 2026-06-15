e8bf6b90d3404249861572ff0818236afea10789
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-06-15 15:28:04 +0000

    feat: add exception

---
+++ src/main/java/com/financedomain/auth/exception/BadFormatAuthenticationException.java
@@ -0,0 +1,7 @@
+package com.financedomain.auth.exception;
+
+public class BadFormatAuthenticationException extends RuntimeException {
+    public BadFormatAuthenticationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/financedomain/auth/exception/NullAuthentificationException.java
@@ -0,0 +1,7 @@
+package com.financedomain.auth.exception;
+
+public class NullAuthentificationException extends RuntimeException {
+    public NullAuthentificationException(String message) {
+        super(message);
+    }
+}
